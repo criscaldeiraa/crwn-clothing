1fc488997f52bc63ae0fdfd22668c617b1d57ebe
criscaldeiraa <criscaldeirat@gmail.com>
2024-02-14 19:22:17 +0000

    correcting styles in directory-item

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 
-import { DirectoryItemContainer, BackgroundImage, DirectoryItemBody, Titleh2, Text } from './directory-item.styles.jsx';
+import { BackgroundImage, Body, DirectoryItemContainer } from './directory-item.styles';
 
 const DirectoryItem = ({ category }) => {
     const { imageUrl, title, route } = category;
@@ -9,15 +9,13 @@ const DirectoryItem = ({ category }) => {
     const onNavigateHandler = () => navigate(route);
 
     return (
-        <DirectoryItemContainer onClick={onNavigateHandler}>
-          <BackgroundImage style={{
-            backgroundImage: `url(${imageUrl})`
-          }} />
-          <DirectoryItemBody>
-            <Titleh2>{title}</Titleh2>
-            <Text>Shop Now</Text>
-          </DirectoryItemBody>
-        </DirectoryItemContainer>
+      <DirectoryItemContainer>
+        <BackgroundImage imageUrl={imageUrl} />
+        <Body>
+          <h2>{title}</h2>
+          <p>Shop Now</p>
+        </Body>
+      </DirectoryItemContainer>
     )
 }
 
